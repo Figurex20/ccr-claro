261a371ff4f735ec6bdb0b48290d2ed196735d65
FIGUREX <kennethggm12@gmail.com>
2023-05-07 08:47:20 -0600

    i made Calendar component also i installed @types/react-datepicker

--- src/generalComponents/Calendar.tsx
+++ src/generalComponents/Calendar.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
-export const Calendar = (setDate: any) => {
+export const Calendar = (props:any) => {
+  const { setDate, newDate } = props
+
+  const [startDate, setStartDate] = useState(newDate)
+
+  setDate(startDate)
+
   return (
-    <div>calendar</div>
+    <div className='d-flex  '>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => {
+          setStartDate(date)
+          setDate(date)
+        }}
+        timeInputLabel='Time:'
+        dateFormat='dd/MM/yyyy h:mm aa'
+        showTimeInput
+        locale='es'
+        isClearable
+        className=' border border-3 form-control'
+      />
+    </div>
   )
 }
