4ce1f35bb6bbbc5c590ff6fd4224bde168900340
KENNETH ALONSO GOMEZ MARTINEZ <kennethggm12@gmail.com>
2023-06-09 13:48:23 -0600

    i made the GET method user in API user/index

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,11 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { dbConnect } from '../utils'
 import { UserController } from '../controllers/user.contollers'
+import NextCors from 'nextjs-cors'
 
 export default async function PUT (req: NextApiRequest, res:NextApiResponse) {
   await dbConnect()
   const { method } = req
   if (method === 'PUT') {
+    const methods = ['PUT']
+    await NextCors(req, res, {
+      methods,
+      origin: '*',
+      optionsSuccessStatus: 200
+    })
+    try {
+      const response:any = await UserController.userChangepassword(req, res)
+      console.log(response)
+      if (response.status === 400) throw Error(response.message)
+      if (response.status === 200) {
+        res.status(200).json({ message: response.message, status: response.status })
+      }
+    } catch (error) {
+      const result = (error as DOMException).message
+      return res.status(500).json({ message: result, status: 500 })
+    }
+  }
+  if (method === 'GET') {
+    const methods = ['GET']
+    await NextCors(req, res, {
+      methods,
+      origin: '*',
+      optionsSuccessStatus: 200
+    })
     try {
       const response:any = await UserController.userChangepassword(req, res)
       console.log(response)
