d7edcde6652b9393e5d439f1ad902242d026fc16
FIGUREX <kennethggm12@gmail.com>
2023-10-29 09:04:31 -0600

    i made the button file, it is working

--- src/pages/informationSites/modalCreateMoreInformationSites.tsx
+++ src/pages/informationSites/modalCreateMoreInformationSites.tsx
@@ -1,15 +1,15 @@
 import { InformationSite } from '@/interface/interfaces'
-import { informationSiteController } from '@/slices/informationSites/informationSiteSlices'
+// import { informationSiteController } from '@/slices/informationSites/informationSiteSlices'
 import React, { useState } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 import { SubmitHandler, useForm } from 'react-hook-form'
 // import { InformationSite } from '../../interface/interfaces'
-
+import * as XLSX from 'xlsx'
 export const ModalCreateMoreInformationSites = () => {
   const {
     register,
     handleSubmit,
-    reset,
+    // reset,
     formState: { errors }
   } = useForm<any>()
 
@@ -17,16 +17,53 @@ export const ModalCreateMoreInformationSites = () => {
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
-  const onSubmit: SubmitHandler<InformationSite> = async (data) => {
-    try {
-      const success = await informationSiteController.createInformationSite(data)
-      if (success) {
-        reset()
-        return
+  // onchange states
+  const [excelFile, setExcelFile] = useState<any>(null)
+  const [typeError, setTypeError] = useState<any>(null)
+
+  // submit state
+  const [excelData, setExcelData] = useState<any>(null)
+
+  const handleFile = (e:any) => {
+    const fileTypes = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv']
+    console.log('e: ', e)
+    const selectedFile = e.target.files[0]
+    if (selectedFile) {
+      if (fileTypes.includes(selectedFile.type)) {
+        setTypeError(null)
+        const reader = new FileReader()
+        reader.readAsArrayBuffer(selectedFile)
+        reader.onload = (e) => {
+          setExcelFile(e.target!.result)
+        }
+      } else {
+        setTypeError('Please select only excel file types')
+        setExcelFile(null)
       }
-    } catch (error) {
-      console.log(error)
+    } else {
+      console.log('Please select your file')
     }
+  }
+
+  const onSubmit: SubmitHandler<InformationSite> = async (data:any) => {
+    if (excelFile !== null) {
+      const workbook = XLSX.read(excelFile, { type: 'buffer' })
+      const worksheetName = workbook.SheetNames[0]
+      const worksheet = workbook.Sheets[worksheetName]
+      const data = XLSX.utils.sheet_to_json(worksheet)
+      console.log('data: ', data)
+      setExcelData(data.slice(0, 10))
+    }
+
+    // try {
+    //   const success = await informationSiteController.createInformationSite(excelData)
+    //   if (success) {
+    //     reset()
+    //     return
+    //   }
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
 
   return (
@@ -47,16 +84,18 @@ export const ModalCreateMoreInformationSites = () => {
               <Form.Control
                 type='file'
                 placeholder='Enter file'
-                {...register('file', { required: true })}
+                {...register('file', { required: true, onChange: (e) => { handleFile(e) } })}
               />
+              <button type='submit' className='btn btn-success btn-md'>UPLOAD</button>
             </Form.Group>
             {errors.file && <h5>El archivo es requerido</h5>}
-
+            {typeError && (
+              <div className='alert alert-danger' role='alert'>{typeError}</div>
+            )}
             <Button className='col-md-12' variant='primary' type='submit' id='submit'>
-              Guardar Usuario
+              Actualizar
             </Button>
           </Form>
-
         </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
@@ -67,6 +106,38 @@ export const ModalCreateMoreInformationSites = () => {
           </Button>
         </Modal.Footer>
       </Modal>
+      {/* view data */}
+      {/* <div className='viewer'>
+        {excelData
+          ? (
+            <div className='table-responsive'>
+              <table className='table'>
+
+                <thead>
+                  <tr>
+                    {Object.keys(excelData[0]).map((key) => (
+                      <th key={key}>{key}</th>
+                    ))}
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {excelData.map((individualExcelData, index) => (
+                    <tr key={index}>
+                      {Object.keys(individualExcelData).map((key) => (
+                        <td key={key}>{individualExcelData[key]}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+
+              </table>
+            </div>
+            )
+          : (
+            <div>No File is uploaded yet!</div>
+            )}
+      </div> */}
     </>
   )
 }
